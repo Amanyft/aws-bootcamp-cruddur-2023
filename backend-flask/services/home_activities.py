cdc7c59d4be339b70f7263e272571f352f30b42f
Ameni feteh <amanyfetah@gmail.com>
2023-03-18 21:11:14 +0000

    updates

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -4,9 +4,8 @@ from opentelemetry import trace
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  def run():
-    with tracer.start_as_current_span("home-activities-mock-data"):
-       
+  def run():  
+    with tracer.start_as_current_span("home-activities-mock-data"): 
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
